c1b8d8bd136a65c6d23d65b5b4d19a689c962c71
YogeshGupta033 <gupta.yogesh033@gmail.com>
2026-03-23 22:56:31 +0530

    02-calsssupdate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-
-    <div>
-    
-    <p>
-      Hello world
-    </p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+        <Button variant="elevated">i am a button</Button>
+      </div>
+      <div>
+        <Input placeholder="i am an input" />        
+      </div>
+      <div>
+        <Progress value={50}/>
+      </div>
+      <div>
+        <Textarea placeholder="i am a textarea" />
+      </div>
+      <div>
+        <input type="checkbox" name="CheckBox " id="" />
+      </div>
     </div>
-    
+    </div>
   );
 }
--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -14,14 +14,16 @@ function Progress({
     <ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
-        "relative flex h-1 w-full items-center overflow-x-hidden rounded-full bg-muted",
+        "relative h-2 w-full  overflow-hidden rounded-full",
+        //Modified Classes
+        "border bg-transparent",
         className
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="size-full flex-1 bg-primary transition-all"
+        className="bg-pink-400 w-full h-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
